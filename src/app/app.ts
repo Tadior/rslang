0cc12a4271a3797474a7bca61867693bbb8233f7
Ilona <mitsunaki@yandex.ru>
2022-08-22 17:09:33 +0300

    refactor: delete test classes in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import Header from './views/Header';
 import Footer from './views/Footer';
-import Games from './views/Games';
-// import MainPage from './views/MainPage';
+import MainPage from './views/MainPage';
 
 const header = new Header();
 header.renderHeader();
-// const mainPage = new MainPage();
-// mainPage.renderMainPage();
-const game = new Games();
-game.renderSprintGame();
+const mainPage = new MainPage();
+mainPage.renderMainPage();
 const footer = new Footer();
 footer.renderFooter();
