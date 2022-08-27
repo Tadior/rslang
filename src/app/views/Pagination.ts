a972bb06f65e67e92db3ec570b7120614a40f299
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-08-27 11:02:57 +0300

    refactor: update renderPagination method

--- src/app/views/Pagination.ts
+++ src/app/views/Pagination.ts
@@ -1,37 +1,13 @@
+import TutorialControllers from '../controllers/TutorialControllers';
+
 export default class Pagination {
   renderPagination() {
-    function createPaginationButton(pageValue: number, isActive = false) {
-      const pageClasses: string[] = ['pagination__btn'];
-      if (isActive) {
-        pageClasses.push('pagination__btn_active');
-      }
-      const button = document.createElement('button');
-      button.classList.add(...pageClasses);
-      button.textContent = String(pageValue);
-      return button;
-    }
-
     const pagination = document.createElement('nav');
     pagination.classList.add('pagination');
-    const paginationPoints = document.createElement('div');
-    paginationPoints.classList.add('pagination__points');
-    paginationPoints.textContent = '...';
-    const prevButton = document.createElement('button');
-    prevButton.classList.add('pagination__btn', 'pagination__btn_prev', 'pagination__btn_disabled');
-    prevButton.textContent = 'Prev';
-    const nextButton = document.createElement('button');
-    nextButton.classList.add('pagination__btn', 'pagination__btn_next');
-    nextButton.textContent = 'Next';
-    pagination.append(
-      prevButton,
-      createPaginationButton(1, true),
-      createPaginationButton(2),
-      createPaginationButton(3),
-      createPaginationButton(4),
-      paginationPoints,
-      createPaginationButton(30),
-      nextButton,
-    );
+    // Добавляем контейнер пагинации на страницу
     document.querySelector('.tutorial__wrapper').after(pagination);
+    // Отрисосываем пагинацию
+    const tutorialController = new TutorialControllers();
+    tutorialController.createPagination(30, 1);
   }
 }
