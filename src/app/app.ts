c5f26e091427b152dd9da555f8b36d7303d83c6c
Ilona <mitsunaki@yandex.ru>
2022-08-28 12:39:51 +0300

    refactor: delete test instances from app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,10 @@
-// import Games from './views/Games';
 import Header from './views/Header';
-import SprintControllers from './controllers/SprintControllers';
-// import Footer from './views/Footer';
-// import MainPage from './views/MainPage';
+import Footer from './views/Footer';
+import MainPage from './views/MainPage';
 
 const header = new Header();
 header.renderHeader();
-// const games = new Games();
-// games.renderSprintGame();
-const controller = new SprintControllers();
-controller.startSprintPage('1', '2', 'd97d52cc-b35e-42fa-becf-8c2ab8caec57');
-/* const mainPage = new MainPage();
+const mainPage = new MainPage();
 mainPage.renderMainPage();
 const footer = new Footer();
-footer.renderFooter(); */
+footer.renderFooter();
