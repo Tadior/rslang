4ac6b999ee2773981ce9d14753c5852d1ef12513
Ilona <mitsunaki@yandex.ru>
2022-09-02 12:03:45 +0300

    feat: create GameMenuControllers

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import Header from './views/Header';
 import Footer from './views/Footer';
 import AuthorizationControllers from './controllers/AuthorizationControllers';
 import MainPage from './views/MainPage';
-// import HeaderControllers from './controllers/HeaderControllers';
 
 const header = new Header();
 header.renderHeader();
@@ -12,6 +11,3 @@ const mainPage = new MainPage();
 mainPage.renderMainPage();
 const footer = new Footer();
 footer.renderFooter();
-
-/* const nav = new HeaderControllers();
-nav.listenHeader(); */
--- src/app/controllers/GameMenuControllers.ts
+++ src/app/controllers/GameMenuControllers.ts
@@ -6,17 +6,22 @@ export default class GameMenuControllers {
   sprintControllers: SprintControllers;
 
   mainPage: MainPage;
-  
+
   footer: Footer;
+
+  group: string;
 
   constructor() {
     this.sprintControllers = new SprintControllers();
     this.mainPage = new MainPage();
     this.footer = new Footer();
+    this.group = '0';
   }
 
   listenGameMenu() {
     this.listenCancelBtn();
+    this.listenLevelsBtns();
+    this.listenStartBtn();
   }
 
   listenCancelBtn() {
@@ -25,6 +30,36 @@ export default class GameMenuControllers {
       document.querySelector('main').innerHTML = '';
       this.mainPage.renderMainPage();
       this.footer.renderFooter();
+    });
+  }
+
+  listenStartBtn() {
+    const cancelBtn = document.querySelector('.btn_start');
+    const gameTitle = document.querySelector('.title_corner');
+    cancelBtn.addEventListener('click', () => {
+      document.querySelector('main').innerHTML = '';
+      if (gameTitle.innerHTML === 'Спринт') {
+        this.sprintControllers.startSprintMenu(this.group);
+      } else {
+        console.log('start audiocall');
+      }
+    });
+  }
+
+  listenLevelsBtns() {
+    const levels = document.querySelector('.difficulty__levels');
+    const startBtn: HTMLButtonElement = document.querySelector('.btn_start');
+    levels.addEventListener('click', (event: Event) => {
+      const target: HTMLElement = event.target as HTMLElement;
+      if (target.classList.contains('difficulty__level')) {
+        this.group = target.getAttribute('data-group');
+        const prevActive = document.querySelector('.active');
+        if (prevActive) {
+          prevActive.classList.remove('active');
+        }
+        target.classList.add('active');
+        startBtn.disabled = false;
+      }
     });
   }
 }
