59b585117ce34946e0bcf2489960f0923f908648
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-09-04 14:46:14 +0300

    feat: create adaptive layout for results and difficulty switcher

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import Header from './views/Header';
 import Footer from './views/Footer';
 import AuthorizationControllers from './controllers/AuthorizationControllers';
 import Tutorial from './views/Tutorial';
-// import MainPage from './views/MainPage';
 
 const header = new Header();
 header.renderHeader();
@@ -10,7 +9,5 @@ const authorizationControllers = new AuthorizationControllers();
 authorizationControllers.enableAuthorization();
 const tutorial = new Tutorial();
 tutorial.renderTutorialPage();
-// const mainPage = new MainPage();
-// mainPage.renderMainPage();
 const footer = new Footer();
 footer.renderFooter();
