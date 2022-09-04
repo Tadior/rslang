35481c18d4a84e4732303622de96454193f4e49c
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-09-04 15:07:44 +0300

    fix: fix burger links bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import Header from './views/Header';
 import Footer from './views/Footer';
 import AuthorizationControllers from './controllers/AuthorizationControllers';
-import Tutorial from './views/Tutorial';
+import MainPage from './views/MainPage';
 
 const header = new Header();
 header.renderHeader();
 const authorizationControllers = new AuthorizationControllers();
 authorizationControllers.enableAuthorization();
-const tutorial = new Tutorial();
-tutorial.renderTutorialPage();
+const main = new MainPage();
+main.renderMainPage();
 const footer = new Footer();
 footer.renderFooter();
